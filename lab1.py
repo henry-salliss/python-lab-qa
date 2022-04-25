dcbb9e6badce44b2a1d3d71130faf6cdf5be1b5c
henry.salliss <henrysalliss@gmail.com>
2022-04-25 11:20:56 +0100

    first commit

---
+++ lab1.py
@@ -0,0 +1,8 @@
+# name = 'henry'
+
+# print("Name is" ,name)
+
+name = input('enter your name:\t')
+age = int(input('enter your age:\t'))
+
+print(f'hello {name} you are {age} years old and you will be {str(age+1)} next year')
